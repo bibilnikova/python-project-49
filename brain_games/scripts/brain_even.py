ce2a7660df872328b9631460f2cd5bf5ba77171c
bibilnikova <bibilnikova@gmail.com>
2023-10-16 22:32:55 +0300

    edit brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -10,23 +10,23 @@ from brain_games.scripts.brain_games import greet
 
 def even_or_odd():
     name = welcome_user()
-    counter = 0
+    flag = True
     correct_ans = ''
     print('Answer "yes" if the number is even, otherwise answer "no"')
-    while counter < 3:
+    for _ in range(3):
         num = random.randint(1, 100)
         correct_ans = 'yes' if num % 2 == 0 else 'no'
         print(f'Question: {num}')
         answer = prompt.string('Your answer: ')
         if answer.lower() == correct_ans:
             print('Correct!')
-            counter += 1
         else:
             print(f'"{answer}" is wrong answer ;(. '
                   f'Correct answer was "{correct_ans}".'
                   f'\nLet\'s try again!')
+            flag = False
             break
-    if counter == 3:
+    if flag:
         print(f'Congratulation, {name}!')
 
 
