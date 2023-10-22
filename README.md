0928f2933108a1d1cbadb50b1f3a0e5b9511dc2d
bibilnikova <bibilnikova@gmail.com>
2023-10-22 16:37:55 +0300

    step 7: add brain-gcd

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 <a href="https://codeclimate.com/github/bibilnikova/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/db0ecf0026348f0b5abc/maintainability" /></a>
 
 brain-games + brain-even: https://asciinema.org/a/uDA97XVjdMY4AQTJAwQDyZVpI
+
 brain-calc: https://asciinema.org/a/wBrYsdVRfatWFQTKMow5IWv3q
+
+brain-gcd: https://asciinema.org/a/v1utgPOkebYC5AA3rizrjEzxj
---
+++ brain_games/scripts/games/brain_gcd.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import random
+from brain_games.scripts.brain_games import greet
+from ..game_logic import game_loop
+
+
+def gcd():
+    num1, num2 = random.randint(1, 50), random.randint(1, 50)
+    question = f'Question {num1} {num2}'
+    while num1 != 0 and num2 != 0:
+        if num1 > num2:
+            num1 %= num2
+        else:
+            num2 %= num1
+    correct_ans = num1 + num2
+    return question, correct_ans
+
+
+def play():
+    sentance = 'Find the greatest common divisor of given numbers.'
+    ans_type = 'integer'
+    return game_loop(gcd, sentance, ans_type)
+
+
+def main():
+    greet()
+    play()
